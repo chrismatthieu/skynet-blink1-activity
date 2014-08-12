29055d0318169040a951d9d48ec816575bf7093e
Chris Matthieu <chris@matthieu.us>
2014-08-12 08:30:25 -0700

    now making blink1 calls locally

--- server.js
+++ server.js
@@ -1,51 +1,35 @@
-var skynet = require('skynet');
+var request = require('request');
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://activity.octoblu.com');
 
-var conn = skynet.createConnection({
-  "uuid": "613f8b90-c9a8-11e3-aa24-e1cb42c326cd",
-  "token": "0b84dh9iedp0q9f6rcqub3kfuy0ysyvi",
-  "server": "ws://meshblu.octoblu.com", // optional - defaults to ws://skynet.im
-  "port": 80  // optional - defaults to 80
-});
+ws.on('message', function(data, flags) {
+  console.log('MESSAGE', data);
+  data = JSON.parse(data);
+  if(data.geo.region == "AZ") {
+    var color = "#0000FF";
+  } else if(data.geo.country == "US"){
+    var color = "#00FF00";
+  } else {
+    var color = "#FF0000";
+  }
 
-conn.on('ready', function(data){
-  console.log('UUID AUTHENTICATED!');
-  console.log(data);
+  request.get('http://127.0.0.1:8934/blink1/fadeToRGB',
+    {qs: {'rgb': color}}
+    , function (error, response, body) {
+    // console.log(body);
+  });
 
-  var WebSocket = require('ws');
-  var ws = new WebSocket('ws://activity.octoblu.com');
+  setTimeout(function(){
 
-  ws.on('message', function(data, flags) {
-    console.log('MESSAGE', data);
-    data = JSON.parse(data);
-    if(data.geo.region == "AZ") {
-      var color = "#0000FF"
-    } else if(data.geo.country == "US"){
-      var color = "#00FF00"
-    } else {
-      var color = "#FF0000"
-    }
-    conn.message({
-      "devices": "feadee3e-7cb5-4fb5-93bd-1bcdba8de1c5",
-      "subdevice": "blink1",
-      "payload": {
-        "on":true,
-        "rgb": color
-      }
-    });
+    request.get('http://127.0.0.1:8934/blink1/fadeToRGB',
+      {qs: {'rgb': '#000000'}}
+      , function (error, response, body) {
+      // console.log(body);
+    });    
+  },500);
 
-    setTimeout(function(){
-      conn.message({
-        "devices": "feadee3e-7cb5-4fb5-93bd-1bcdba8de1c5",
-        "subdevice": "blink1",
-        "payload": {
-          "on":false
-        }
-      });
-    },500);
+});  
 
-  });  
-
-});
 
 
 
